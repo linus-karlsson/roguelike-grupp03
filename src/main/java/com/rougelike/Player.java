6a2d1768301635bab79116a8639cd7407b533a53
linus-klk <linus.karlssonlk33@gmail.com>
2023-10-09 14:45:25 +0200

    Började med Player klassen

---
+++ src/main/java/com/rougelike/Player.java
@@ -0,0 +1,36 @@
+package com.rougelike;
+
+public class Player {
+    private static final int MAX_LEVEL = 60;
+
+    private String name;
+    private Race race;
+
+    private double health;
+
+    private int level;
+    private double xp;
+    private double xpToNextLevel;
+
+    public Player(String name, Race race) {
+        this.name = name;
+        this.race = race;
+        xpToNextLevel = 200.0;
+    }
+
+    // ??????
+    public void nextLevel() {
+        xpToNextLevel *= 3.5;
+    }
+
+    public void increaseXp(double gainedXp) {
+        double xpOverspill = (xp + gainedXp) - xpToNextLevel;
+        if (xpOverspill > 0) {
+            // TODO: MAX_LEVEL
+            assert level < MAX_LEVEL;
+            level += 1;
+            nextLevel();
+        }
+        xp += gainedXp;
+    }
+}
