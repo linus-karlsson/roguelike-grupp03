8b26b0265559d69d90984171345e503bfb81a486
UglyWallach <david.sundstrom94@outlook.com>
2023-10-09 18:19:55 +0200

    Utkast till Race-klass

---
+++ src/main/java/com/rougelike/races/Race.java
@@ -0,0 +1,39 @@
+package com.rougelike.races;
+
+public abstract class Race {
+    private double health;
+    private double mana;
+    private double strength;
+    private double dexterity;
+    private double intelligence;
+
+    public Race(double health, double mana, double strength, double dexterity, double intelligence) {
+        this.health = health;
+        this.mana = mana;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public double getMana() {
+        return mana;
+    }
+
+    public double getStrength() {
+        return strength;
+    }
+
+    public double getIntelligence() {
+        return intelligence;
+    }
+
+    public double getDexterity() {
+        return dexterity;
+    }
+
+
+}
---
+++ src/test/java/com/rougelike/races/RaceTest.java
@@ -0,0 +1,15 @@
+package com.rougelike.races;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+public class RaceTest {
+    private static final double DWARF_STARTING_HEALTH = 100.0;
+
+    @Test
+    public void TestDwarfInstanceCreation(){
+        Dwarf dwarf = new Dwarf();
+        assertEquals(DWARF_STARTING_HEALTH, dwarf.getHealth());
+    }
+}
