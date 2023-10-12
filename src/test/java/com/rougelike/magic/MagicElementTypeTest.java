825a5ef2bfb00776e935c56b25cf5bb7277bbcea
tokr7092 <tomas.kronnas@gmail.com>
2023-10-12 17:37:12 +0200

    test med testsuite klass fungerar lite halvt

--- src/test/java/com/rougelike/magic/MagicElementTypeTest.java
+++ src/test/java/com/rougelike/magic/MagicElementTypeTest.java
@@ -2,11 +2,13 @@ package com.rougelike.magic;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MagicElementTypeTest {
 
     @Test
+    @DisplayName("Test MagicElementType instance creation")
     void testGetMultiplierReturnsCorrectValueWhenNeutral() {
         Magic magic = new Magic(Spell.HEAL);
         double expectedMultiplier = 1.0;
--- src/test/java/com/rougelike/magic/RunAllMagicTest.java
+++ src/test/java/com/rougelike/magic/RunAllMagicTest.java
@@ -1,16 +1,17 @@
 package com.rougelike.magic;
 
-import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
 
 @Suite
+@RunWith(org.junit.platform.runner.JUnitPlatform.class)
 @SuiteDisplayName("Magic Tests")
 @SelectPackages("com.rougelike.magic")
-@SelectClasses({SpellTest.class, MagicTest.class, MagicElementTypeTest.class})
+@IncludeClassNamePatterns({ "^.*Test$", "^.*Tests$" })
 
- class RunAllMagicTest {
+class RunAllMagicTest {
 
-    }
-
+}
