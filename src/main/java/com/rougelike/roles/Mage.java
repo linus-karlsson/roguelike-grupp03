780ed9e485b4bc80a4b8b2f698749718d3130923
UglyWallach <david.sundstrom94@outlook.com>
2023-10-17 09:59:09 +0200

    Starting weapon i roller

--- src/main/java/com/rougelike/roles/Mage.java
+++ src/main/java/com/rougelike/roles/Mage.java
@@ -1,6 +1,9 @@
 package com.rougelike.roles;
 
 import com.rougelike.equipment.EquipmentType;
+import com.rougelike.equipment.Sword;
+import com.rougelike.equipment.Wand;
+import com.rougelike.equipment.Weapon;
 
 public class Mage extends Role {
     private static final double MAGE_HEALTH_MULTIPLIER = 1.0;
@@ -9,8 +12,10 @@ public class Mage extends Role {
     private static final double MAGE_DEXTERITY_MULTIPLIER = 1.0;
     private static final double MAGE_INTELLIGENCE_MULTIPLIER = 2.0;
 
+    private static final Weapon MAGE_STARTING_WEAPON = new Wand();
+
     public Mage(){
-        super(MAGE_HEALTH_MULTIPLIER, MAGE_MANA_MULTIPLIER, MAGE_STRENGTH_MULTIPLIER, MAGE_DEXTERITY_MULTIPLIER, MAGE_INTELLIGENCE_MULTIPLIER, EquipmentType.LIGHT_ARMOR, EquipmentType.WAND);
+        super(MAGE_HEALTH_MULTIPLIER, MAGE_MANA_MULTIPLIER, MAGE_STRENGTH_MULTIPLIER, MAGE_DEXTERITY_MULTIPLIER, MAGE_INTELLIGENCE_MULTIPLIER, MAGE_STARTING_WEAPON, EquipmentType.LIGHT_ARMOR, EquipmentType.WAND);
 
     }
 
--- src/main/java/com/rougelike/roles/Thief.java
+++ src/main/java/com/rougelike/roles/Thief.java
@@ -1,6 +1,9 @@
 package com.rougelike.roles;
 
+import com.rougelike.equipment.Dagger;
 import com.rougelike.equipment.EquipmentType;
+import com.rougelike.equipment.Sword;
+import com.rougelike.equipment.Weapon;
 
 public class Thief extends Role {
 
@@ -10,8 +13,9 @@ public class Thief extends Role {
     private static final double THIEF_DEXTERITY_MULTIPLIER = 1.8;
     private static final double THIEF_INTELLIGENCE_MULTIPLIER = 1.0;
 
+    private static final Weapon THIEF_STARTING_WEAPON = new Dagger();
     public Thief(){
-        super(THIEF_HEALTH_MULTIPLIER, THIEF_MANA_MULTIPLIER,THIEF_STRENGTH_MULTIPLIER,THIEF_DEXTERITY_MULTIPLIER,THIEF_INTELLIGENCE_MULTIPLIER, EquipmentType.LIGHT_ARMOR, EquipmentType.MEDIUM_ARMOR, EquipmentType.DAGGER);
+        super(THIEF_HEALTH_MULTIPLIER, THIEF_MANA_MULTIPLIER,THIEF_STRENGTH_MULTIPLIER,THIEF_DEXTERITY_MULTIPLIER,THIEF_INTELLIGENCE_MULTIPLIER, THIEF_STARTING_WEAPON, EquipmentType.LIGHT_ARMOR, EquipmentType.MEDIUM_ARMOR, EquipmentType.DAGGER);
     }
 
     @Override
