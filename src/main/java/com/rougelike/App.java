53c1589ea9e90a8c2bdcb990f61da01d391d1b97
linus-klk <linus.karlssonlk33@gmail.com>
2023-10-18 18:11:46 +0200

    Room.getPosition() return en kopia av sin plats

--- src/main/java/com/rougelike/App.java
+++ src/main/java/com/rougelike/App.java
@@ -4,22 +4,5 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        double minWidth = 10.0;
-        double maxWidth = 30.0;
-        double minHeight = 10.0;
-        double maxHeight = 30.0;
-
-        Map map = new Map();
-        int roomCount = 50;
-        ArrayList<Room> rooms = map.generateListOfRooms(roomCount, minWidth, maxWidth,
-                minHeight, maxHeight);
-
-        int rows = 40;
-        int columns = 40;
-        ArrayList<Room> placedRooms = map.placeRoomsInArea(rooms, 30, rows, columns);
-        map.connectRooms(placedRooms);
-        Gridd gridd = map.getCopyOfGridd();
-        int[] connectedRooms = Graph.getConnectedRooms(placedRooms, gridd);
-        map.printGridd();
     }
 }
