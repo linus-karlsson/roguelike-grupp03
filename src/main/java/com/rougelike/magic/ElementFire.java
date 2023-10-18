f6926d9948219aa7882d800e60eef19c83910936
tokr7092 <tomas.kronnas@gmail.com>
2023-10-18 11:47:50 +0200

    uppdaterat med getMulti.. metoder för framtida utveckling

--- src/main/java/com/rougelike/magic/ElementFire.java
+++ src/main/java/com/rougelike/magic/ElementFire.java
@@ -1,4 +1,6 @@
 package com.rougelike.magic;
+
+import com.rougelike.*;
 //Orch stakare, alv svagare
 // Eld är stark mot vind(vind) och jord (dvärg) (starkare vid kombination med vind)
 // Eld påverkar orcher svagare och mot vatten
@@ -6,6 +8,11 @@ public class ElementFire extends MagicElementType   {
 
     public ElementFire() {
         super("Fire");
+    }
+
+    @Override
+    public double getMultiplier(Player player) {
+        return BASE_MULTIPLIER;
     }
     
 }
--- src/main/java/com/rougelike/magic/ElementWater.java
+++ src/main/java/com/rougelike/magic/ElementWater.java
@@ -1,5 +1,7 @@
 package com.rougelike.magic;
 
+import com.rougelike.*;
+
 //dvärg svagare
 //vatten påverkar vind(alv) starkare i kombination med jord
 //vatten påverkar eld(orch) svagare
@@ -7,5 +9,10 @@ public class ElementWater extends MagicElementType {
         
             public ElementWater() {
                 super("Water");
+            }
+
+            @Override
+            public double getMultiplier(Player player) {
+                return BASE_MULTIPLIER;
             }
 }
