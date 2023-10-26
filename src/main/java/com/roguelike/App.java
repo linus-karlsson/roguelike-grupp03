bfc33e3b1b2e6d1d30d41f54afa06c0781582beb
linus-klk <linus.karlssonlk33@gmail.com>
2023-10-26 09:26:43 +0200

    Ny profiler setup

--- src/main/java/com/roguelike/App.java
+++ src/main/java/com/roguelike/App.java
@@ -2,7 +2,6 @@ package com.roguelike;
 
 import java.util.*;
 
-import com.roguelike.dungeon.DungeonGenerator;
 import com.roguelike.races.Human;
 import com.roguelike.roles.Mage;
 import com.roguelike.enemies.*;
@@ -12,18 +11,6 @@ import com.roguelike.magic.Spell;
 
 public class App {
     public static void main(String[] args) {
-        /*
-         * double minWidth = 30.0;
-         * double maxWidth = 60.0;
-         * double minHeight = 30.0;
-         * double maxHeight = 60.0;
-         * int numberOfTriesBeforeDiscard = 30;
-         * int columnCount = 1000;
-         * int rowCount = 1000;
-         * double tileSize = DungeonGenerator.MIN_TILE_SIZE;
-         * DungeonGenerator dungeonGenerator = new DungeonGenerator();
-         * int roomCount = 1000;
-         */
         List<Entity> enemies = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             enemies.add(new Bandit());
@@ -43,9 +30,6 @@ public class App {
         final double duration = 30.0;
         while (sec <= duration) {
             long start = System.nanoTime();
-            // dungeonGenerator.generateDungeon(roomCount, minWidth, maxWidth,
-            // minHeight, maxHeight, numberOfTriesBeforeDiscard,
-            // rowCount, columnCount, tileSize);
             long end = System.nanoTime() - start;
             sec += (double) end / 1_000_000_000.0;
         }
