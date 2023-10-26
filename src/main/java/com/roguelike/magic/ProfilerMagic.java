f57224cd53aed51a650f0bd2076dff67a851c3ec
tokr7092 <tomas.kronnas@gmail.com>
2023-10-26 07:59:56 +0200

    uppdaterat profiler

--- src/main/java/com/roguelike/magic/ProfilerMagic.java
+++ src/main/java/com/roguelike/magic/ProfilerMagic.java
@@ -8,9 +8,7 @@ import com.roguelike.roles.Mage;
 
 import java.util.ArrayList;
 
-
 public class ProfilerMagic {
-
 
     public static void main(String[] args) {
         ArrayList<Bandit> bandits = new ArrayList<>();
@@ -18,19 +16,24 @@ public class ProfilerMagic {
         Player player = new Player("TestPlayer", new Elf(), new Mage(), new Point2D(1, 1));
         player.setLevel(2);
         player.addMagicToInventory(new Magic(Spell.TORNADO));
-        for (int i = 0; i < 1000000; i++) {
-            bandits.add(new Bandit());
-        }
-        for (Bandit bandit : bandits) {
-            System.out.println(bandit.getHealth());
-
-            do {
-                player.useMagic(Spell.TORNADO, bandit);
+        double sec = 0.0;
+        final double duration = 30.0;
+        while (sec <= duration) {
+            long start = System.nanoTime();
+            for (int i = 0; i < 1000000; i++) {
+                bandits.add(new Bandit());
+            }
+            for (Bandit bandit : bandits) {
                 System.out.println(bandit.getHealth());
-            } while (!bandit.isDead());
-            System.out.println(bandit.isDead());
+
+                do {
+                    player.useMagic(Spell.TORNADO, bandit);
+                    System.out.println(bandit.getHealth());
+                } while (!bandit.isDead());
+                System.out.println(bandit.isDead());
+                long end = System.nanoTime() - start;
+                sec += (double) end / 1_000_000_000.0;
+            }
         }
-    }
-}
 
-
+    }}
